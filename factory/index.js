0ee68e87a4d64c500573dfe1da225f6cd5087c82
fescnet <fescnet@yahoo.com.br>
2023-08-07 21:04:50 -0300

    Delete factory directory

--- factory/index.js
+++
@@ -1,9 +0,0 @@
-const UserFactory = require('./UserFactory')
-
-const manager = UserFactory.createUser('Mary', 'Manager')
-manager.addEmployee('Jacob')
-manager.addEmployee('John')
-const employee = UserFactory.createUser('John', 'Employee')
-
-manager.display()
-employee.display()
